1a33545cfcee2a9196862a2deed9f10740ccb097
Matias Ezequiel Martirian <131602278+MattMartirian@users.noreply.github.com>
2025-06-18 20:03:19 -0300

    Recarga del proyecto

---
+++ Proyecto/Codigo_ProyectoDAW/SERVICIOS/Sesion.cs
@@ -0,0 +1,45 @@
+﻿using BE;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERVICIOS
+{
+    public class Sesion
+    {
+        public Usuario Usuario;
+        private static Sesion instancia;
+        public static Sesion Instancia
+        {
+            get
+            {
+                if (instancia == null) instancia = new Sesion();
+                return instancia;
+            }
+        }
+
+        public void LogIn(Usuario pUsuario)
+        {
+            Usuario = pUsuario;
+        }
+        public void LogOut()
+        {
+            if (Usuario != null)
+            {
+                Usuario = null;
+            }
+        }
+        public bool IsLogueado()
+        {
+            return Usuario == null ? false : true;
+        }
+
+        public bool Verificar(string pUsuario, string pContra)
+        {            
+            return UsuarioDAL.Instancia.ValidarUsuario(pUsuario, pContra);
+        }
+    }
+}
