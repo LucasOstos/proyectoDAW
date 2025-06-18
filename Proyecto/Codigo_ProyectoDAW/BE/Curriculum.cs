642d3013f1984eed46b009f11db1ddc4dbd38922
LucasOstos <131553773+LucasOstos@users.noreply.github.com>
2025-06-18 13:27:07 -0300

    Login

---
+++ Proyecto/Codigo_ProyectoDAW/BE/Curriculum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Curriculum
+    {
+        public int Num_Curriculum { get; set; }
+        public Usuario ID_Usuario { get; set; }
+        public string Titulo { get; set; }
+        public string Experiencia { get; set; }
+        public string Educacion { get; set; }
+        public string Habilidades { get; set; }
+
+        public Curriculum(int numCurriculum, Usuario pUsuario, string pTitulo, string pExperiencia, string pEducacion, string pHabilidades)
+        {
+            Num_Curriculum = numCurriculum;
+            ID_Usuario = pUsuario;
+            Titulo = pTitulo;
+            Experiencia = pExperiencia;
+            Educacion = pEducacion;
+            Habilidades = pHabilidades;
+        }
+    }
+}
---
+++ Proyecto/Codigo_ProyectoDAW/DAL/UsuarioDAL.cs
@@ -0,0 +1,66 @@
+﻿using BE;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class UsuarioDAL
+    {
+        private static UsuarioDAL instancia;
+        public static UsuarioDAL Instancia
+        {
+            get
+            {
+                if (instancia == null) { instancia = new UsuarioDAL(); }
+                return instancia;
+            }
+        }
+        public bool ValidarUsuario(string pUsuario, string Contra)
+        {
+            bool x = false;
+            string query = $"SELECT * FROM Usuario WHERE username = @username AND contraseña = @contraseña";
+            using (SqlCommand CM = new SqlCommand(query, Conexion.Instancia.ReturnConexion()))
+            {
+                CM.Parameters.AddWithValue("@username", pUsuario);
+                CM.Parameters.AddWithValue("@contraseña", Contra);
+                Conexion.Instancia.AbrirConexion();
+                using (SqlDataReader DR = CM.ExecuteReader())
+                {
+                    while (DR.Read())
+                    {
+                        if (DR != null)
+                        {
+                            x = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            Conexion.Instancia.CerrarConexion();
+            return x;
+        }
+        public Usuario ObtenerUsuario(string pUsuario)
+        {
+            Usuario U = null;
+            string Query = $"SELECT * FROM Usuario WHERE username = @username";
+            using (SqlCommand CM = new SqlCommand(Query, Conexion.Instancia.ReturnConexion()))
+            {
+                Conexion.Instancia.AbrirConexion();
+                CM.Parameters.AddWithValue("@username", pUsuario);
+                using (SqlDataReader DR = CM.ExecuteReader())
+                {
+                    if (DR.Read())
+                    {
+                        U = new Usuario(int.Parse(DR[0].ToString()), DR[1].ToString(), DR[2].ToString(), int.Parse(DR[3].ToString()), DR[4].ToString(), DR[5].ToString(), DR[6].ToString());                        
+                    }
+                }
+            }
+            Conexion.Instancia.CerrarConexion();
+            return U;
+        }
+    }
+}
