0b6b654286db78af649c9d23a99d12fc1976f255
LucasOstos <131553773+LucasOstos@users.noreply.github.com>
2025-07-06 16:30:36 -0300

    diagramas y algo mas creo

---
+++ Proyecto/Diagramas/DC/PaginaPrincipal/FormPeliculas.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BE;
+using BLL;
+
+public partial class FormPeliculas : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        CargarNombres();
+        CargarPeliculas();
+    }
+    private void CargarPeliculas()
+    {
+        gvPeliculas.DataSource = null;
+        GestorPeliculas gestor = new GestorPeliculas();
+        List<Pelicula> peliculas = gestor.Peliculas();
+        gvPeliculas.DataSource = peliculas;
+        gvPeliculas.DataBind();
+    }
+    private void CargarNombres()
+    {
+        ddlPeliculas.Items.Clear();
+        GestorPeliculas gestor = new GestorPeliculas();
+        List<Pelicula> peliculas = gestor.Peliculas();
+        foreach (Pelicula p in peliculas)
+        {
+            ddlPeliculas.Items.Add(p.Nombre);
+        }
+    }
+
+    protected void btnBorrar_Click(object sender, EventArgs e)
+    {
+        GestorPeliculas gestor = new GestorPeliculas();
+        gestor.BorrarPelicula(ddlPeliculas.Text);
+        CargarPeliculas();
+        CargarNombres();
+    }
+}
