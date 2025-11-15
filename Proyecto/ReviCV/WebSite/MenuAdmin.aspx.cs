f0aa46fae9564e63d2f4dc905dcfc87f51998fde
BrunoSola8 <bruno.sola@alumnos.uai.edu.ar>
2025-11-15 16:12:47 -0300

    probando
    mmm

--- Proyecto/ReviCV/WebSite/MenuAdmin.aspx.cs
+++ Proyecto/ReviCV/WebSite/MenuAdmin.aspx.cs
@@ -10,6 +10,7 @@ public partial class MenuAdmin : Page, IObserver
     protected void btnInicio_Click(object sender, EventArgs e)
     {
         Response.Redirect("MenuAdmin.aspx");
+        string probando;
     }
 
     protected void btnUsuarios_Click(object sender, EventArgs e)
