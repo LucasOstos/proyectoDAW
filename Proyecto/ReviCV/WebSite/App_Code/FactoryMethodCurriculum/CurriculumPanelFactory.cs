068fc70e2c8a9a76ae615f7c44374e06361fe9c3
Matias Ezequiel Martirian <131602278+MattMartirian@users.noreply.github.com>
2025-11-16 17:28:33 -0300

    Factory Method agregado

---
+++ Proyecto/ReviCV/WebSite/App_Code/FactoryMethodCurriculum/CurriculumPanelFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+public abstract class CurriculumFactory
+{
+    public abstract Panel CrearPanel(Curriculum cv);
+}
